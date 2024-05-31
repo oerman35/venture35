a0aaa48e64670bdccdcb1441d0a1e30bcc8fea7f
oerman35 <morne@redrubyit.co.za>
2024-05-31 22:40:02 +0200

    Added contact form, instagram and styling fixes

---
+++ assets/js/contact.js
@@ -0,0 +1,29 @@
+﻿document.getElementById('contact-form').addEventListener('submit', function (event) {
+    event.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    const data = {
+        name: name,
+        email: email,
+        message: message
+    };
+
+    fetch('https://maker.ifttt.com/trigger/venture35_contact_form/json/with/key/deaGSAOp-rqf60HoumGb8T', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(response => response.json())
+        .then(json => {
+            alert('Your message has been sent!');
+            document.getElementById('contact-form').reset();
+        })
+        .catch(error => {
+            console.error('Error:', error);
+            alert('There was an error sending your message. Please try again.');
+        });
+});
