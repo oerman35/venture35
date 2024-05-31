5f2206bbbf7682766f81ab2c8d71aaed72d01691
oerman35 <morne@redrubyit.co.za>
2024-05-31 22:59:00 +0200

    Contact form

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -4,16 +4,20 @@
     const email = document.getElementById('email').value;
     const message = document.getElementById('message').value;
 
+    const data = {
+        'name': name,
+        'email': email,
+        'message': message
+    };
+
+    console.log('data', data);
+
     fetch('https://maker.ifttt.com/trigger/venture35_contact_form/json/with/key/deaGSAOp-rqf60HoumGb8T', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: {
-            'name': name,
-            'email': email,
-            'message': message
-        }
+        body: JSON.stringify(data)
     })
         .then(response => response.json())
         .then(json => {
