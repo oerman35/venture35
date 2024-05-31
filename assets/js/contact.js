7b4f5c763ac349620b223fd25c991ad4f8ab53cb
oerman35 <morne@redrubyit.co.za>
2024-05-31 23:15:18 +0200

    Testing webhook site

--- assets/js/contact.js
+++ assets/js/contact.js
@@ -12,7 +12,7 @@
 
     console.log('data', data);
 
-    fetch('https://maker.ifttt.com/trigger/venture35_contact_form/json/with/key/deaGSAOp-rqf60HoumGb8T', {
+    fetch('https://webhook.site/8a4cba53-bd90-4735-970f-a4348c811eab', {
         method: 'POST',
         headers: {
             "Accept": "application/json, text/plain, */*",
